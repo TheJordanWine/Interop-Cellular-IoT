2051f3882b6778871d0c3947e864299e774b7fa0
nick-okerberg <nicholas.okerberg@asu.edu>
2018-10-29 15:00:00 -0500

    Tsk54: Updated TestUtilityMeter to provide more output to console when executed.

--- client/src/TestUtilityMeter.cpp
+++ client/src/TestUtilityMeter.cpp
@@ -8,7 +8,7 @@
  * to see if expected values are returned.  It echos the result of each
  * test.
  *
- * Instructions: 
+ * Instructions:
  * Compile this test file along with the UtilityMeter implementation, then
  * execute the binary file using the steps below:
  *    1) Compile:  g++ -o main UtilityMeter.cpp TestUtilityMeter.cpp
@@ -32,6 +32,11 @@ int main (int argc, char* argv[]) {
 
   cout << "\nTesting UtilityMeter.cpp Implementation file...\n";
 
+  // Variables in main
+  int meterValue;         // The reading value of the meter.
+  bool powerStatus;       // The power status of the Utility Meter.
+  int setValue;           // The value to set the meter to.
+
   // Overall test result True = Pass, False = Fail. Initialize to True.
   bool finalResult = true;
 
@@ -39,52 +44,78 @@ int main (int argc, char* argv[]) {
   UtilityMeter* u1;
 
   // Create a UtilityMeter object using default constructor.
+  cout << "   * Creating UtilityMeter object.....";
   u1 = new UtilityMeter();
+  cout << "Done!\n";
 
   // The meter value should be zero to start with.
-  if (u1->getMeterValue() != 0) {
+  cout << "   * Starting meter value should be zero:  ";
+  meterValue = u1->getMeterValue();
+  cout << meterValue << "\n";
+  if (meterValue != 0) {
     finalResult = false;
   }
 
   // The meter should be powered on to start with.
-  if (u1->getPowerStatus() != true) {
+  cout << "   * The Utility Meter should be powered on:  ";
+  powerStatus = u1->getPowerStatus();
+  cout << powerStatus << "\n";
+  if (powerStatus != true) {
     finalResult = false;
   }
 
-  // Set the meter value to a positive value > 0.
-  int setValue = 555;
+  // Set the meter value to a positive value > 0 and read the meter.
+  setValue = 555;
+  cout << "   * Setting meter to value:  " << setValue << "\n";
   u1->setMeterValue(setValue);
-  if (u1->getMeterValue() != setValue) {
+  meterValue = u1->getMeterValue();
+  cout << "     Current meter value reading:  " << meterValue << "\n";
+  if (meterValue != setValue) {
     finalResult = false;
   }
 
   // Set the meter value to a negative value.
   // The UtilityMeter is designed to use value 0 in such cases.
-  int setNegValue = -555;
-  u1->setMeterValue(setNegValue);
-  if (u1->getMeterValue() != 0) {
+  setValue = -555;
+  cout << "   * Setting meter to neg value:  " << setValue << "\n";
+  u1->setMeterValue(setValue);
+  meterValue = u1->getMeterValue();
+  cout << "     Current meter value, should be zero:  " << meterValue << "\n";
+  if (meterValue != 0) {
     finalResult = false;
   }
 
   // Set the meter value to 0.
+  cout << "   * Testing setting meter to value zero:  ";
   u1->setMeterValue(0);
-  if (u1->getMeterValue() != 0) {
+  meterValue = u1->getMeterValue();
+  cout << meterValue << "\n";
+  if (meterValue != 0) {
     finalResult = false;
   }
 
   // Reset the meter value, it should be set to 0.
+  cout << "   * Testing the meter value reset function...\n";
+  cout << "     Set meter to value:  ";
   u1->setMeterValue(333);
+  meterValue = u1->getMeterValue();
+  cout << meterValue << "\n";
+  cout << "     Initiated Reset...now value is: ";
   u1->resetMeterValue();
+  meterValue = u1->getMeterValue();
+  cout << meterValue << "\n";
   if (u1->getMeterValue() != 0) {
     finalResult = false;
   }
 
   // Test the random value generator. The value should be between 0-1000.
+  cout << "   * Testing Random meter value generator. Values should be 0-1000: \n";
   int i = 0;            // Iterator.
   int numTests = 10;    // Number of times to Iterate (Num of tests).
   int ranValue;
   while (i < numTests) {
     ranValue = u1->getRandomValue();
+    cout << "     Iteration# " << i+1 << " Value: " << ranValue << "\n";
     if ( (ranValue < 0) | (ranValue > 1000) ) {
       finalResult = false;
     }
@@ -92,14 +123,28 @@ int main (int argc, char* argv[]) {
   }
 
   // Ensure the Meter can be powered off.
+  cout << "   * Testing setPowerStatus function to turn the meter off...\n";
+  powerStatus = u1->getPowerStatus();
+  cout << "     Current power status: " << powerStatus << "\n";
+  cout << "     Turning off meter...\n";
   u1->setPowerStatus(false);
+  powerStatus = u1->getPowerStatus();
+  cout << "     Current power status: " << powerStatus << "\n";
   if (u1->getPowerStatus() != false) {
     finalResult = false;
   }
 
+  // Print the overall result.
   cout << "\nThe final test result:  ";
-  cout << finalResult;
+  if (finalResult != false) {
+    cout << "PASSED!";
+  }
+  else {
+    cout << "FAILED!";
+  }
+  cout << "\n";
 
+  // Return
   return 0;
 
 } // End of Main
