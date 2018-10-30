461f0a168024f9495630effcd24df3d890853524
nick-okerberg <nicholas.okerberg@asu.edu>
2018-10-30 01:11:10 -0500

    Tsk70 Created Container and CI for meter values to be stored

--- client/src/MeterMain.cpp
+++ client/src/MeterMain.cpp
@@ -1,6 +1,9 @@
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "onem2m.hxx"
+#include "UtilityMeter.h"
 
 using namespace std;
 
@@ -15,41 +18,87 @@ int main (int argc, char* argv[]) {
   string loginCred = "admin:admin";     // The OM2M Server login credentials.
   string aeName = "MY_METER";           // Name of the AE Resource to create.
   string aeAppId = "app1";              // Name of the AE App Id. Mandatory.
+  string contName = "meter-value";      // Container Name.
   ::xml_schema::integer respObjType;    // The response data from server.
   string cseRootAddr = "/in-cse/in-name";            // SP-Relative address.
   std::unique_ptr< ::xml_schema::type > respObj;     // The result code from server.
+  UtilityMeter um;                      // Construct our UtilityMeter object.
+  int meterValue;                       // Represents Utility Meter Value.
+  string meterValueStr;                 // Utility Meter Value as a string.
 
-  // First, initialize the library.
+ /*
+  * First, initialize the OS-IoT library.
+  */
   cout << "\nInitializing oneM2M library...";
   onem2m::initialize();
   cout << "Done!\n";
 
-  // Update the values for pre-set parameters.
+  /*
+   * Update the values for pre-set parameters.
+   */
   cout << "Updating pre-set parameters...";
   onem2m::setHostName(hostName);    // OM2M server address.
-  onem2m::setFrom(loginCred);           // Credentials.
+  onem2m::setFrom(loginCred);       // Credentials.
   cout << "Done!\n";
 
-  // Create the MY_METER AE resource object and push to OM2M server.
+  /*
+   * Create the main MY_METER AE resource object and push to OM2M server.
+   */
   cout << "Creating MY_METER AE resource object...\n";
   auto aeMyMeter = onem2m::AE();
   aeMyMeter.resourceName(aeName);
   aeMyMeter.App_ID(aeAppId);
   aeMyMeter.requestReachability(false); // requestReachability is mandatory for AE.
-  respObj = onem2m::createResource(cseRootAddr, "5555", aeMyMeter, result, respObjType);
+  respObj = onem2m::createResource(cseRootAddr, "5555", aeMyMeter,
+    result, respObjType);
   cout << "Result code is:  " << result << "\n";
 
-  // Get the resource
+  /*
+   * Retrieve the AE resource.  For testing.
+   */
   cout << "\nRetrieving AE resource object...\n";
-  respObj = ::onem2m::retrieveResource("/in-cse/in-name/MY_METER", "5556", result, respObjType);
+  respObj = ::onem2m::retrieveResource(cseRootAddr+"/"+aeName,
+    "5555", result, respObjType);
   cout << "Result = " << result << "\n";
   cout << "respObjType = " << respObjType << "\n";
 
-  // Close the library.
+  /*
+   * Create a Container in our AE. This container will store the meter value.
+   */
+  cout << "\nCreating Container...\n";
+  auto cnt = ::onem2m::container();
+  cnt.resourceName(contName);
+  respObj = ::onem2m::createResource(cseRootAddr+"/"+aeName,
+    "5555", cnt, result, respObjType);
+  cout << "\nContainer creation result code: " << result << "\n";
+
+  /*
+   * Write simulated utility meter value data to Content Instance in the Container
+   */
+  cout << "\nCreating Content Instance...\n";
+  auto contInst = ::onem2m::contentInstance();
+  contInst.contentInfo("application/text");      // Text data.
+  meterValue = um.getRandomValue();              // Get a simulated value from meter.
+  meterValueStr = to_string(meterValue);         // Convert int to Str.
+  contInst.content(meterValueStr);               // Write simulated value.
+  respObj = ::onem2m::createResource(cseRootAddr+"/"+aeName+"/"+contName,
+    "5555", contInst, result, respObjType);
+
+  /*
+   * Get the Content Instance resource. For testing.
+   */
+  cout << "\nRetrieving Content Instance resource object...\n";
+  respObj = ::onem2m::retrieveResource(cseRootAddr+"/"+aeName+"/"+contName,
+    "5555", result, respObjType);
+  cout << "Result = " << result << "\n";
+  cout << "respObjType = " << respObjType << "\n";
+
+  /*
+   * Close the OS-IoT library and exit.
+   */
   ::onem2m::terminate();
   cout << "Finished!\n";
 
-  // Exit
   return 1;
 
 } // End of main
