a4ab095956097ce1bf54a7d4122d3a8b44f16848
Jordan Wine <jordandwine@gmail.com>
2018-11-23 19:24:04 -0800

    Task49 - Generated server key/cert and configured OM2M server for https

--- server/serverCertificate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-apt-get install -y openssl;
-openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "/C=US/ST=Arizona/L=Tempe/O=Arizona State University/OU=Capstone Team 6/CN=osiot.com";
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt;
