3c76e6cf55838eee68884dfa70c0334aa68b0b2f
nick-okerberg <nicholas.okerberg@asu.edu>
2018-11-26 04:11:45 -0600

    TG-200 Simple Hello World Example in C for testing an app on BG96

---
+++ client_c/src/ExampleHelloWorld.c
@@ -0,0 +1,33 @@
+/**
+ * Example Hello World File for C:  ExampleHelloWorld.c
+ *
+ * This file is a simple Hello World example C program.  It is
+ * used to demonstrate that the BG96 board can run a simple C
+ * application.
+ *
+ * Instructions:
+ *     1) Compile using gcc:
+ *        gcc ExampleHelloWorld.c
+ *     2) Test, execute:
+ *        ./a.out
+ *     3) Copy to BG96 and execute. 
+ *
+ * @author Team 6
+ *
+ * @version 1.0
+ */
+
+ // Imports
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ /**
+   * Main function, program entry point.
+   */
+ int main (int argc, char* argv[]) {
+
+   printf("\nHello World!\n");
+
+   return 0;
+
+ } // End of main
