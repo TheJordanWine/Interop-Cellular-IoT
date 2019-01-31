5d85e81d65413bf70ebdc84fb9012877afa8fdce
Jordan Wine <jordandwine@gmail.com>
2019-01-30 21:45:03 -0800

    TG-147 adding node version to travis.yml instead of shell script

--- webapp/install-dependencies.sh
+++ webapp/install-dependencies.sh
@@ -3,8 +3,5 @@
 # Installation script for dependencies. This is designed to be used by Travis for continous integration
 cd webapp
 
-#Install nodejs
-sudo apt-get install g++ build-essential -y
-
 # Install webapp dependencies
 npm install
