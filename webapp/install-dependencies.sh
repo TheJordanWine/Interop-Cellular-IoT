2f61c6fea87d455a007fe5959fbef87498f2254c
Jordan Wine <jordandwine@gmail.com>
2019-01-30 21:38:46 -0800

    TG-147 adding node install to Travis install script

--- webapp/install-dependencies.sh
+++ webapp/install-dependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Installation script for dependencies. This is designed to be used by Travis for continous integration
-
 cd webapp
+
+#Install nodejs
+sudo apt-get install g++ build-essential -y
+
+# Install webapp dependencies
 npm install
