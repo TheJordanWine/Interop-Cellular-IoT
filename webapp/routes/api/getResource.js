53be269c80eeb5d24aad4d325809e86a42e00151
Shalom <Ovadia.Shalom@verint.com>
2019-02-01 13:31:51 -0600

    TG-150 transfer api routes

---
+++ webapp/routes/api/getResource.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var fs = require('fs');
+module.exports = function(app, isAuthenticatedCustomMiddleware,serverOpt) {
+    app.get('/api/get/:ae', isAuthenticatedCustomMiddleware, function(req, res) {
+        var resourceName = req.params.ae;
+        console.log('test??');
+        if(serverOpt.AE_NAMES.includes(resourceName)) {
+            if(fs.existsSync(resourceName)) {
+                var dataFile = path.resolve(__dirname + '../../../' + resourceName + '/data.json');
+                console.log('filename:', dataFile);
+                res.sendfile(dataFile);
+            }else {
+                res.statusCode = 500;
+                res.send('null');
+                res.end();
+            }
+        }else {
+            res.statusCode = 400;
+            res.send('null');
+            res.end();
+        }
+    });
+}
