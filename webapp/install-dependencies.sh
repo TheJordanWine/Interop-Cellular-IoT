c272c15603f93f011aa02e320ccd2adb857601d7
Jordan Wine <jordandwine@gmail.com>
2019-02-02 11:00:49 -0800

    TG-148 adding java install for OM2M server

--- webapp/install-dependencies.sh
+++ webapp/install-dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install java 8
+apt-get install openjdk-8-jdk -y
+
 # Installation script for dependencies. This is designed to be used by Travis for continous integration
 cd webapp
 
