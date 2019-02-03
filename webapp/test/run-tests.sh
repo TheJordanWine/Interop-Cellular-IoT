73a68e335fb3f65008010d9e100d508899e2b0fb
Jordan Wine <jordandwine@gmail.com>
2019-02-02 16:18:11 -0800

    TG-148 fixing chrome path

--- webapp/test/run-tests.sh
+++ webapp/test/run-tests.sh
@@ -37,6 +37,8 @@ if ls webapp >/dev/null; then
     google-chrome-stable --no-sandbox --headless --disable-gpu --disable-dev-shm-usage --remote-debugging-port=9222 http://localhost &
 
     # Run tests
+    echo "chcking for chrome"
+    ps -ef |grep google-chrome
     npm test
 
 
