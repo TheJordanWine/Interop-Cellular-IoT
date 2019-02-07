cdba50180de15d04da80dbf2ad93be861657dc4a
Jordan Wine <jordandwine@gmail.com>
2019-02-06 21:41:16 -0800

    TG-158 updating connection to mongo to use authentication

--- webapp/createUsers.js
+++ webapp/createUsers.js
@@ -1,36 +1,46 @@
-var MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient,
+  f = require("util").format;
 
 //
 //  config
 //
+var mongoPort = "27017";
+var mongoHost = "localhost";
+var dbName = "cellulariot";
 
-var mongoPort = '27017'
-var mongoHost = 'localhost'
+// Configure the credentials for DEFAULT authentication with the mongo DB
+var user = encodeURIComponent("myUserAdmin");
+var password = encodeURIComponent("abc123");
+var authMechanism = "DEFAULT";
 
-var dbName = 'cellulariot'
-var userName = 'admin'
-var userPassword = 'admin'
+// Connection URL
+var mongoUrl = f(
+  "mongodb://%s:%s@" +
+    mongoHost +
+    ":" +
+    mongoPort +
+    "/" +
+    dbName +
+    "?authMechanism=%s",
+  user,
+  password,
+  authMechanism
+);
 
 //
 //  start
 //
+MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+  if (err) {
+    console.log(err.message);
+    return console.log("Error: could not connect to mongodb");
+  }
+  console.log("database created");
 
-MongoClient.connect('mongodb://' + mongoHost + ':' + mongoPort + '/' + dbName,
-    function (err, db) {
-
-        if (err) {
-            return console.log('Error: could not connect to mongodb')
-        }
-
-
-        // Authenticate using the newly added user
-        db.authenticate(userName, userPassword, function (err, result) {
-
-            if (err) {
-                return console.log('Error: could authenticate with created user')
-            }
-
-            console.log('Ok')
-            db.close()
-        })
-    })
+  var dbo = db.db(dbName);
+  dbo.createCollection("test", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
--- webapp/setupDatabase.sh
+++ webapp/setupDatabase.sh
@@ -1,11 +1,11 @@
-mongod --dbpath=$PWD/database
-# db.createUser({
-#     "user": "admin",
-#     "pwd": "admin",
-#     "roles": [
-#         {
-#             "role": "userAdminAnyDatabase",
-#             "db": "admin"
-#         }
-#     ]
-# });
+# TODO Finish setup of database and user creation.
+#mongod --dbpath=$PWD/database
+#use admin
+#db.createUser(
+#  {
+#    user: "myUserAdmin",
+#    pwd: "abc123",
+#    roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
+#  }
+#)
+#mongod --auth --port 27017 --dbpath $PWD/database
