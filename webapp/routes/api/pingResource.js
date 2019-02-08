b314b3d2d8c49b0d706be475031088a02fa1c21d
Shalom <Ovadia.Shalom@verint.com>
2019-02-08 12:02:52 -0600

    TG-170 added ability to ping http server

--- webapp/routes/api/pingResource.js
+++ webapp/routes/api/pingResource.js
@@ -2,25 +2,29 @@ var path = require('path');
 var fs = require('fs');
 var request = require('request');
 module.exports = function (app, isAuthenticatedCustomMiddleware, serverOpt) {
-    /**
-     * Will trigger bidirectional call to Client
-     */
-    request.post(`${isHttps == "true" ? "https" : "http"}://${om2mhost}:${om2mport}/~/in-cse/in-name/MY_SENSOR/PING_METER`, {
-        headers: {
-            "content-type": "application/json;ty=4",
-            "x-m2m-origin": "admin:admin"
-        },
-        body: JSON.stringify({
-            "m2m:cin": {
-                cnf: "application/text",
-                con: '{"function": "getCurrentMeterReading" }'
+
+    app.post('/ping', function(req, res) {
+        /**
+         * Will trigger bidirectional call to Client
+         */
+        request.post(`${isHttps == "true" ? "https" : "http"}://${om2mhost}:${om2mport}/~/in-cse/in-name/MY_SENSOR/PING_METER`, {
+            headers: {
+                "content-type": "application/json;ty=4",
+                "x-m2m-origin": "admin:admin"
+            },
+            body: JSON.stringify({
+                "m2m:cin": {
+                    cnf: "application/text",
+                    con: '{"function": "getCurrentMeterReading" }'
+                }
+            })
+            },
+            function (err, resp, body) {
+                if (err) {
+                    throw new Error("Unable to post data. ");
+                }
             }
-        })
-        },
-        function (err, resp, body) {
-            if (err) {
-                throw new Error("Unable to post data. ");
-            }
-        }
-    );
+        );    
+    });
+    
 };
