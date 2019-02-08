244a954da647d987022096203d60660719cf06d8
Shalom <Ovadia.Shalom@verint.com>
2019-02-08 11:52:10 -0600

    TG-169 added request to module to ping container

--- webapp/routes/api/pingResource.js
+++ webapp/routes/api/pingResource.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var fs = require('fs');
-module.exports = function(app, isAuthenticatedCustomMiddleware,serverOpt) {
+var request = require('request');
+module.exports = function (app, isAuthenticatedCustomMiddleware, serverOpt) {
     /**
      * Will trigger bidirectional call to Client
      */
+    request.post(`${isHttps == "true" ? "https" : "http"}://${om2mhost}:${om2mport}/~/in-cse/in-name/MY_SENSOR/PING_METER`, {
+        headers: {
+            "content-type": "application/json;ty=4",
+            "x-m2m-origin": "admin:admin"
+        },
+        body: JSON.stringify({
+            "m2m:cin": {
+                cnf: "application/text",
+                con: '{"function": "getCurrentMeterReading" }'
+            }
+        })
+        },
+        function (err, resp, body) {
+            if (err) {
+                throw new Error("Unable to post data. ");
+            }
+        }
+    );
 };
