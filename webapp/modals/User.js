2554a120b2d6bab3ef406f528cf6f950828d7060
Shalom <Ovadia.Shalom@verint.com>
2019-02-13 22:29:15 -0600

    TG-190 added modal for mongoose

---
+++ webapp/modals/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+mongoose.connect('mongodb://localhost/nodeauth')
+
+
+//User Schema
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String
+    }
+})
+
+const User = module.exports = mongoose.model('User', UserSchema)
+
+module.exports.getUserById = (id, cb) => {
+    User.findById(id, cb);
+};
+
+module.exports.getUserByUsername = (username, cb) => {
+    var query = {
+        username: username
+    };
+    User.findOne(query, cb);
+};
+module.exports.comparePassword = (candidatePassword, hash, cb) => {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        cb(null, isMatch);
+    });
+};
+module.exports.createUser = (newUser, cb) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            newUser.password = hash;
+            newUser.save(cb);
+        });
+    });
+};
