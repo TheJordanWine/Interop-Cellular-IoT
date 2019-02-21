e0b69cf7e586a61411167d475594bba12006e3f3
Shalom <Ovadia.Shalom@verint.com>
2019-02-20 22:16:09 -0600

    TG-207 added createMyMeter to clib demo

--- client_c/src/MeterMain_c_libcurl.c
+++ client_c/src/MeterMain_c_libcurl.c
@@ -24,6 +24,8 @@
 
 // Forward Declrations 
 void getResource();
+void createMyMeter();
+
 /**
    * Main function, program entry point.
    */
@@ -31,6 +33,9 @@ int main(int argc, char *argv[])
 {
     //Request to the om2m Server
     getResource();
+
+    //Creates an AE MY_METER
+    createMyMeter();
     
     return 0;
 }
@@ -51,6 +56,31 @@ void getResource()
     CURLcode ret = curl_easy_perform(hnd);
     /* Perform the request, res will get the return code */
     /* Check for errors */
+    if (ret != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(ret));
+    }
+
+    /* always cleanup */
+    curl_easy_cleanup(hnd);
+}
+
+void createMyMeter()
+{
+    printf("Creating my meter....\n");
+    CURL *hnd = curl_easy_init();
+
+    curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(hnd, CURLOPT_URL, "http://192.168.0.8:8080/~/in-cse");
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "x-m2m-origin: admin:admin");
+    headers = curl_slist_append(headers, "content-type: application/xml;ty=2");
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "<m2m:ae xmlns:m2m=\"http://www.onem2m.org/xml/protocols\" rn=\"MY_METER\" ><api>app-sensor</api><lbl>Type/sensor Category/temperature Location/home</lbl><rr>false</rr></m2m:ae>");
+
+    CURLcode ret = curl_easy_perform(hnd);
     if (ret != CURLE_OK)
     {
         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(ret));
