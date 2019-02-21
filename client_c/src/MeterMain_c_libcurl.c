1153f4df2eaa3f77f19543de41a8486abe622636
Shalom <Ovadia.Shalom@verint.com>
2019-02-20 22:17:16 -0600

    TG-207 added createDescriptionContainer function to clib demo

--- client_c/src/MeterMain_c_libcurl.c
+++ client_c/src/MeterMain_c_libcurl.c
@@ -25,7 +25,7 @@
 // Forward Declrations 
 void getResource();
 void createMyMeter();
-
+void createDescriptorContainer();
 /**
    * Main function, program entry point.
    */
@@ -36,6 +36,9 @@ int main(int argc, char *argv[])
 
     //Creates an AE MY_METER
     createMyMeter();
+    
+    //Creates a descriptor contianer for MY_METER
+    createDescriptorContainer();
     
     return 0;
 }
@@ -79,6 +82,29 @@ void createMyMeter()
     curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
 
     curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "<m2m:ae xmlns:m2m=\"http://www.onem2m.org/xml/protocols\" rn=\"MY_METER\" ><api>app-sensor</api><lbl>Type/sensor Category/temperature Location/home</lbl><rr>false</rr></m2m:ae>");
+
+    CURLcode ret = curl_easy_perform(hnd);
+    if (ret != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(ret));
+    }
+
+    /* always cleanup */
+    curl_easy_cleanup(hnd);
+}
+void createDescriptorContainer()
+{
+    CURL *hnd = curl_easy_init();
+
+    curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(hnd, CURLOPT_URL, "http://127.0.0.1:8080/~/in-cse/in-name/MY_METER");
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "x-m2m-origin: admin:admin");
+    headers = curl_slist_append(headers, "content-type: application/xml;ty=3");
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "<m2m:cnt xmlns:m2m=\"http://www.onem2m.org/xml/protocols\" rn=\"DESCRIPTOR\"></m2m:cnt>");
 
     CURLcode ret = curl_easy_perform(hnd);
     if (ret != CURLE_OK)
