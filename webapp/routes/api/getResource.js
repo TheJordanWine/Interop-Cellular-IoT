689e1a91bbdc765c812b646383b52205ad8805ec
Shalom <Ovadia.Shalom@verint.com>
2019-03-01 13:18:25 -0600

    TG-202 updated getResrouce route for beta testing

--- webapp/routes/api/getResource.js
+++ webapp/routes/api/getResource.js
@@ -1,23 +1,50 @@
 var path = require('path');
 var fs = require('fs');
-module.exports = function(app, isAuthenticatedCustomMiddleware,serverOpt) {
-    app.get('/api/get/:ae', isAuthenticatedCustomMiddleware, function(req, res) {
+var MongoClient = require('mongodb').MongoClient;
+module.exports = function (app, isAuthenticatedCustomMiddleware, serverOpt) {
+    app.get('/api/get/:ae', isAuthenticatedCustomMiddleware, function (req, res) {
         var resourceName = req.params.ae;
-        console.log('test??');
-        if(serverOpt.AE_NAMES.includes(resourceName)) {
-            if(fs.existsSync(resourceName)) {
-                var dataFile = path.resolve(__dirname + '../../../' + resourceName + '/data.json');
-                console.log('filename:', dataFile);
-                res.sendfile(dataFile);
-            }else {
-                res.statusCode = 500;
+
+        if (process.env.BETA) {
+
+            var url = "mongodb://localhost:27017/";
+
+            MongoClient.connect(url, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("nodeauth");
+                var query = {
+                    date: !!req.query.currentDate ? req.query.currentDate : new Date().toLocaleDateString()
+                };
+                dbo.collection("datas").find(query).toArray(function (err, result) {
+                    if (err) {
+                        res.json({
+                            message: 'error'
+                        });
+                    } else {
+                        console.log(result);
+                        res.json(result);
+                        db.close();
+                    }
+                });
+            });
+            
+        } else {
+            if (serverOpt.AE_NAMES.includes(resourceName)) {
+                if (fs.existsSync(resourceName)) {
+                    var dataFile = path.resolve(__dirname + '../../../' + resourceName + '/data.json');
+                    console.log('filename:', dataFile);
+                    res.sendfile(dataFile);
+                } else {
+                    res.statusCode = 500;
+                    res.send('null');
+                    res.end();
+                }
+            } else {
+                res.statusCode = 400;
                 res.send('null');
                 res.end();
             }
-        }else {
-            res.statusCode = 400;
-            res.send('null');
-            res.end();
         }
+
     });
 }
