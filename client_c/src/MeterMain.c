88c6870f349f67211295471c0c03475dbcd359cd
treser1 <treser@asu.edu>
2019-04-06 15:14:27 -0700

    TG-255 Added descriptor content instance.

--- client_c/src/MeterMain.c
+++ client_c/src/MeterMain.c
@@ -36,7 +36,7 @@
 #include "qapi_psm.h"
 #include <stdlib.h>
 #include <string.h>
-#include "http_demo_utils.h"
+#include "http_utils.h"
 
 // Configuration file for the application - see list of keys below
 #define HTTP_CONFIG_FILE	"/datatx/http_config.json"
@@ -68,8 +68,7 @@
 #define AE_FROM_ID_KEY "ae_from_id" // Required string. "From" value (X-M2M-Origin) for AE
 #define AE_RESOURCE_NAME_KEY "ae_resource_name" // Required string. Resource name to be used for the AE
 #define CONTAINER_RESOURCE_NAME_KEY "container_resource_name" // Required string. Resource name to be used for the container.
-#define CONTAINER_DESCRIPTOR_NAME_KEY "container_descriptor_name"// Required string. Descriptor name to be used for the AE.
-#define CONTAINER_DESCRIPTOR_VALUE_KEY "container_descriptor_value" // Configurable description for AE
+#define DESCRIPTOR_VALUE_KEY "descriptor_value" // Configurable description for AE
 #define APP_ID_KEY "app_id" // Required string. App-ID to be used for the AE resource
 #define DNS_PRI_V4_KEY "dns_pri_v4" // Optional string. Include to set DNS primary server address (if DNS is used).
 #define DNS_SEC_V4_KEY "dns_sec_v4" // Optional string. Include to set DNS secondary server address.
@@ -96,7 +95,7 @@
 
 #define DATA_CALL_ATTEMPTS 2 // Number of times to attempt to setup a data call before waiting until next scheduled report.
 #define DATA_CALL_RETRY_INTERVAL 20 // Interval (seconds) for initial retries if data call fails
-#define HTTP_ATTEMPTS 2 // Number of times to attempt to setup a data call before waiting until next scheduled report.
+#define HTTP_ATTEMPTS 4 // Number of times to attempt to setup a data call before waiting until next scheduled report.
 #define HTTP_RETRY_INTERVAL 20 // Interval (seconds) for initial retries if http request fails
 #define REQUEST_INTERVAL 60 // Time (seconds) between reports
 
@@ -144,6 +143,7 @@ enum op_state_enum  {
 	create_ae,
 	create_container,
 	create_descriptor,
+	populate_descriptor,
 	reporting
 };
 
@@ -610,38 +610,31 @@ qapi_Status_t http_request_using_state() {
 			ATIS_LOG_INFO("Create container request result = %d", status);
 			onem2m_resource_free(res);
 			break;
+		
 		case create_descriptor:
-			//TODO Finish setting this up so that it creates a container and then an instance
-			// if possible set the max number of instances to 1
-			// Create Descriptor container
 			strcpy(&(address[strlen(address)]),"/");
 			strcpy(&(address[strlen(address)]), json_object_get_string(config_object, AE_RESOURCE_NAME_KEY));
 			res = onem2m_resource_allocate_container();
 			if (! res)
 				return QAPI_ERROR;
-			onem2m_container_set_resourceName_string(res, json_object_get_string(config_object, CONTAINER_DESCRIPTOR_NAME_KEY));
-			onem2m_container_set_maxNrOfInstances_number(res, 1); //ensure only one descriptor instance is active at a time
+			onem2m_container_set_resourceName_string(res, "DESCRIPTOR");
 			status = onem2m_http_create(address, "1234", res, http_cb_buffer);
 			ATIS_LOG_INFO("Create container request result = %d", status);
 			onem2m_resource_free(res);
-			
-			// Create the content instance containing the descriptor value
+			break;
+		case populate_descriptor:
 			strcpy(&(address[strlen(address)]),"/");
 			strcpy(&(address[strlen(address)]), json_object_get_string(config_object, AE_RESOURCE_NAME_KEY));
 			strcpy(&(address[strlen(address)]),"/");
-			strcpy(&(address[strlen(address)]), json_object_get_string(config_object, CONTAINER_DESCRIPTOR_NAME_KEY));
+			strcpy(&(address[strlen(address)]), "DESCRIPTOR");
 			res = onem2m_resource_allocate_contentInstance();
 			if (! res)
 				return QAPI_ERROR;
 			onem2m_contentInstance_set_contentInfo_string(res, "application/text");
-			
-			snprintf(content_string, MAX_CONTENT_LEN, json_object_get_string(config_object, CONTAINER_DESCRIPTOR_VALUE_KEY)); // the actual descriptor value
-			ATIS_LOG_INFO("DEBUG: REPORTING TEXT: %s", content_string);
-			onem2m_contentInstance_set_content_string(res, content_string);
+			onem2m_contentInstance_set_content_string(res, json_object_get_string(config_object, DESCRIPTOR_VALUE_KEY));
 			status = onem2m_http_create(address, "1234", res, http_cb_buffer);
 			ATIS_LOG_INFO("Create ContentInstance request result = %d", status);
 			onem2m_resource_free(res);
-			
 			break;
 		case reporting:
 			strcpy(&(address[strlen(address)]),"/");
@@ -683,8 +676,7 @@ qapi_Status_t http_request_with_retry() {
 	UINT i;
 	for (i = 0; i< HTTP_ATTEMPTS; i++) {
 		ATIS_LOG_INFO("Try HTTP request # %d", i+1);
-		status = http_request_using_state();
-		//TODO Add op_state to descriptor creation and make a call to http_request_using_state
+		status = http_request_using_state(); 
 		if (status == QAPI_OK) {
 			status = wait_for_http_session();
 			if (status == QAPI_OK) {
